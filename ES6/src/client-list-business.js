9530bb7212fc03357f0e5e606bdbbaf118141907
Tony <shidadk@hotmail.es>
2022-08-18 15:29:04 +0200

    Finalizacion de imports i exports

---
+++ ES6/src/client-list-business.js
@@ -0,0 +1,13 @@
+import { getClients } from "./data-business";
+import { getClientElement } from "./client-business";
+
+export function printClientsAccounts() {
+  const clients = getClients();
+  const ul = document.createElement("ul");
+  for (let client of clients) {
+    const element = getClientElement(client);
+    ul.appendChild(element);
+  }
+
+  document.getElementById("root").appendChild(ul);
+}
